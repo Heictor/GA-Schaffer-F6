4f67eed3e2c0be8e273a9499301f73773e00cbe2
Heictor <39010251+Heictor@users.noreply.github.com>
2021-04-29 18:12:15 -0300

    Mudanças na mutação

--- GA Code/my_ga2.m
+++ GA Code/my_ga2.m
@@ -10,7 +10,8 @@ function [ best_fitness ,...
            costo,...
            generation_fitness,...
            population_cost,...
-           population_index] = my_ga2 ( ...
+           population_index,...
+           population_size] = my_ga2 ( ...
     number_of_variables , ...     % O número de parâmetros para resolver o problema
     fitness_function , ...        % Nome da função Fitness
     population_size , ...         % Tamanho daa população (número de indivíduos em cada geração)
@@ -24,6 +25,7 @@ function [ best_fitness ,...
 % rng default
 %% Criação das Variáveis Iniciais
 
+cumulative_probabilities = cumsum((parent_number:- 1 : 1 ) / sum(parent_number:- 1 : 1 ));
 %População Inicial
 
 %Para população começando de forma aleatória
@@ -33,7 +35,7 @@ function [ best_fitness ,...
 % 10.
 
 %Para população de ínicio definido
-population = repelem([-5.777057566444397e+06, -2.316175794493771e+07],4,1);
+population = repelem([-5.777057566444397e+06, -2.316175794493771e+07],population_size,1);
 % Cria uma matriz NxM, sendo N = O tamanho da população, e M = Quantidade
 % de variáveis. A matriz vai ser composta da repetição dos valores
 % iniciais, ou seja, valores dos pais.
@@ -43,6 +45,11 @@ population = repelem([-5.777057566444397e+06, -2.316175794493771e+07],4,1);
 first_fitness = feval('my_fitness',[-5.777057566444397e+06 -2.316175794493771e+07]);
 
 best_fitness = [];
+
+% Número de Crianças
+% População de pais de tamanho numerado (pais são indivíduos que não mudam a cada geração)
+child_number = population_size - parent_number; % Número de crianças em cada geração
+
 
 
 %Elite
@@ -66,10 +73,14 @@ for i = 1:1:maximal_generation
     %atual e salvar os valores de fitness eles
     for k = 1:1:population_size
         %population_size irá salvar os valores de fitness de cada indivíduo
-        %dentro da geração atual. Portanto este variável vai armazenar o 
+        %dentro da geração atual. Portanto este variável vai armazenar os
+        %valores de fitness de toda a população de todas as gerações ao
+        %longo da execução
         population_fitness = [population_fitness; feval(fitness_function, [population(k,1) population(k,2)])]; 
         [population_cost, population_index] = sort(population_fitness);
         
+        %Matrix que armazena os valores de fitness de cada indivíduo na
+        %geração "i" atual
         generation_fitness(k,:) = feval(fitness_function, [population(k,1) population(k,2)]);
         [cost, index] = sort(generation_fitness);
         
@@ -77,10 +88,61 @@ for i = 1:1:maximal_generation
           
     end
     
-    best_fitness = [best_fitness; cost(4)]
+    %Melhor valor de fitness em cada geração 
+    best_fitness = [best_fitness; cost(length(cost))];
     
-    %Atualização da População
-    population = 0.5 + (1+1)*rand([population_size, number_of_variables]);
+    %Cruzamento das Indivíduos
+    for child=1:2:child_number
+       mother = population(length(population),:)
+       father = population(length(population)-1,:)
+       crossover_point = ceil(rand*number_of_variables);
+%        mask1 = [ones( 1 , crossover_point), zeros( 1 , number_of_variables - crossover_point)];
+%        mask2 = not(mask1) ;
+       mask1 = [1 0];
+       mask2 = [0 1]; 
+       mother_1 = mask1 .* mother % The front part of the mother chromosome
+       mother_2 = mask2 .* mother % The posterior part of the mother chromosome
+       
+       father_1 = mask1 .* father % The front part of the mother chromosome
+       father_2 = mask2 .* father % The posterior part of the mother chromosome 
+       
+       population(parent_number + child, :) = mother_1 + father_2 % a child
+       population(parent_number+child+ 1 , :) = mother_2 + father_1
+    end    
+    
+    % Chromosome variation starts
+    % Variation population
+    mutation_population = population( 1 :population_size, :); % elites do not participate in mutation, so start from 2
+    
+    number_of_elements = (population_size -  1 ) * number_of_variables; % number of all genes
+    number_of_mutations = ceil(number_of_elements * mutation_rate); % The number of mutated genes (total number of genes * mutation rate)
+    
+    % rand(1, number_of_mutations) Generate a matrix of number_of_mutations random numbers (range 0-1) (1*number_of_mutations)
+    %After multiplication, each element of the matrix represents the position of the changed gene (the one-dimensional coordinate of the element in the matrix)
+    mutation_points = ceil(number_of_elements * rand( 1 , number_of_mutations)); % determine the gene to be mutated
+    
+    % The selected genes are replaced by a random number to complete the mutation
+    mutation_population(mutation_points) = rand( 1 , number_of_mutations);
+    
+    population( 1 :population_size, :) = mutation_population; % population after mutation
+    
+%     % Variation population
+%     mutation_population = population( 1 :population_size, :); % elites do not participate in mutation, so start from 2
+%     
+%     number_of_elements = (population_size ) * number_of_variables; % number of all genes
+%     number_of_mutations = ceil(number_of_elements * mutation_rate); % The number of mutated genes (total number of genes * mutation rate)
+%     
+%     % rand(1, number_of_mutations) Generate a matrix of number_of_mutations random numbers (range 0-1) (1*number_of_mutations)
+%     %After multiplication, each element of the matrix represents the position of the changed gene (the one-dimensional coordinate of the element in the matrix)
+%     mutation_points = ceil(number_of_elements * rand( 1 , number_of_mutations)); % determine the gene to be mutated
+%     
+%     % The selected genes are replaced by a random number to complete the mutation
+%     mutation_population(mutation_points) = rand( 1 , number_of_mutations); % Perform mutation operations on selected genes
+%     population( 1 :population_size, :) = mutation_population
+%     %Atualização da População
+    
+    %population = 0.5 + (1+1)*rand([population_size, number_of_variables]);
+    %population = population+(0.01+(0.01+0.01)*rand)
     
     
     
@@ -89,5 +151,7 @@ for i = 1:1:maximal_generation
     
     
 end
+%disp("A tabela abaixo exibe informações sobre as variáveis utilizadas");
+%whos;
 % generation_fitness = population_fitness(i-3:i,1);
 % [cost, index] = sort(generation_fitness);
--- GA Code/test_ga2.m
+++ GA Code/test_ga2.m
@@ -1,8 +1,8 @@
 clc; 
-% close all;
-% clear all;
+close all;
+clear all;
 % format long
-% rng default
+%rng default
 [best_fitness ,...
  elite , ...
  generation ,...
@@ -14,36 +14,32 @@ clc;
  costo,...
  generation_fitness,...
  population_cost,...
- population_index] = my_ga2 ( ...
+ population_index,...
+ population_size] = my_ga2 ( ...
     2 ,... %Número de Variáveis
     'my_fitness' ,... %Função Fitness
     4 ,... %Tamanho da população
     1 ,... %Número de pais que permanecerão na próxima geração
-    0.01 ,... %Taxa de Mutação
-    10 ,... %Máximo de Gerações
+    0.1 ,... %Taxa de Mutação
+    100,... %Máximo de Gerações
     1.0e-6... %Custo minímo de evolução
 );
 
-for i = 1:4:length(population_fitness)
-    [population_fitness(i,:);population_fitness(i+1,:);population_fitness(i+2,:);population_fitness(i+3,:)];
-    
-end
-
 %% Gráfico para os valores e Fitness ao longo das gerações
-% figure(1)
-% hold on
-% gera = 1:1:40;
-% plot(gera, population_fitness)
-% grid on
-% hold off
-
+figure(1)
+hold on
+gera = 1:1:length(population_fitness);
+scatter(gera, population_fitness)
+grid on
+hold off
+% % 
 %% Gráfico para os valores e Fitness ao longo das gerações
-% figure(2)
-% hold on
-% gera = 1:1:10;
-% scatter(gera, best_fitness)
-% grid on
-% hold off
+figure(2)
+hold on
+gera = 1:1:length(best_fitness);
+scatter(gera, best_fitness)
+grid on
+hold off
 
 
 
